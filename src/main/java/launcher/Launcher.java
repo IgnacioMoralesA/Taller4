f6ed6f72ce65c700383f8bae939a5be9856af9c7
Ignacio Morales <70661952+amphibian2006@users.noreply.github.com>
2022-06-28 10:37:34 -0400

    Delete src/main/java directory

--- src/main/java/launcher/Launcher.java
+++
@@ -1,9 +0,0 @@
-package launcher;
-
-import guis.Ventana_CursoICC264;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Ventana_CursoICC264 ventana_cursoICC264 = new Ventana_CursoICC264("Curso ICC264",500,250);
-    }
-}
